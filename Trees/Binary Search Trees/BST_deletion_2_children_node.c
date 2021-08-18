9d07c48134f6d303b5d011abb7054f4cc759992e
pras5972 <88701260+pras5972@users.noreply.github.com>
2021-08-18 18:52:36 +0530

    Update BST_deletion_2_children_node.c

--- Trees/Binary Search Trees/BST_deletion_2_children_node.c
+++ Trees/Binary Search Trees/BST_deletion_2_children_node.c
@@ -121,6 +121,7 @@ int main()
     {
         insert();
     }
+    printf("\nInorder Traversal of tree: ");
     inOrder(root);
     printf("\n");
 
@@ -130,6 +131,8 @@ int main()
     printf("Enter the node to be deleted: ");
     scanf("%d", &key);
     delete (key);
+    
+    printf("\nInorder Traversal of tree: ");
     inOrder(root);
     return 0;
 }
